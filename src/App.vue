b1511c918907c83addddfa0eb074aa992cb4f6cb
m0slah <muhammedslahqadir@gmail.com>
2023-07-07 23:48:21 +0300

    add html structure

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,30 @@
 <template>
-  <h1>first commit</h1>
+  <div class="ctr">
+    <div class="questions-ctr">
+      <div class="progress">
+        <div class="bar"></div>
+        <div class="status">1 out of 3 questions answered</div>
+      </div>
+      <div class="single-question">
+        <div class="question">Sample Question 1</div>
+        <div class="answers">
+          <div class="answer">Sample Answer 1</div>
+          <div class="answer">Sample Answer 2</div>
+          <div class="answer">Sa mple Answer 3</div>
+          <div class="answer">Sample Answer 4</div>
+        </div>
+      </div>
+    </div>
+    <div class="result">
+      <div class="title">You got sample result 1!</div>
+      <div class="desc">Enter a short description here about the result.</div>
+    </div>
+    <button type="button" class="reset-btn">Reset</button>
+  </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  name: "App",
+};
+</script>
